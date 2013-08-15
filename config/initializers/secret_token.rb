bc70a67762afee32ea5c69a921b2b790627fd0c9
Liu Jiyang <2411mail@gmail.com>
2013-08-15 11:36:28 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imagecrop::Application.config.secret_token = '66cb5690421d9042e04f7df42285198081373604ac2418ce69e9f46c7a4686e0c3738497e87308ed01e76e906b145ad8fffe875a606e1de85dd3a71186c9743f'
