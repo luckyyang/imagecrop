ea5be8d7a5c35262148cf3f941fd6dd132d81c5a
Liu Jiyang <2411mail@gmail.com>
2013-08-15 17:17:52 +0800

    add some auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :normal => "300x300>", :small => "200x200>",  :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
   :url  => "/assets/users/:id/:style/:basename.:extension",
   :path => ":rails_root/public/assets/users/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :avatar
+  validates_attachment_size :avatar, :less_than => 5.megabytes
+  validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/png']
 end
