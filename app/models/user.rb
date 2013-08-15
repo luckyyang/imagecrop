5dd33e50bff8bc91ce88d8b10e03b279fc0112e4
Liu Jiyang <2411mail@gmail.com>
2013-08-15 17:14:48 +0800

    now we can upload images

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :avatar
-    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-            # :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-            # :url => "/system/:attachment/:id/:style/:filename"
+  has_attached_file :avatar, :styles => { :normal => "300x300>", :small => "200x200>",  :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+  :url  => "/assets/users/:id/:style/:basename.:extension",
+  :path => ":rails_root/public/assets/users/:id/:style/:basename.:extension"
 end
